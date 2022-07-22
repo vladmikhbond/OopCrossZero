89abd8a5361b811a7728cdea991d2f46226274b9
volodymyr bondariev <vladmikhbond@hotmail.com>
2022-07-22 08:51:29 +0300

    Add project files.

---
+++ OopCrossZero/Model/Field.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopCrossZero.Model
+{
+   public enum CellState { Empty = 0, Cross = 1, Zero = 10};
+
+   public class Field
+   {
+      public int Size { private set; get;}
+
+      CellState[] cells;
+
+      public Field(int size)
+      {
+         Size = size;
+         cells = new CellState[Size * Size];
+         Array.Fill(cells, CellState.Empty);
+      }
+
+      public CellState this[int r, int c]
+      {
+         set { cells[r * Size + c] = value; }
+         get { return cells[r * Size + c]; }
+      }
+
+      public CellState Turn(string turn, CellState mark)
+      {
+         this[0, 0] = mark;
+         return CellState.Empty;
+      }
+
+   }
+}
---
+++ OopCrossZero/Program.cs
@@ -0,0 +1,22 @@
+﻿using OopCrossZero.Model;
+using OopCrossZero.View;
+
+Field field = new Field(5);
+View view = new View(field);
+
+string turn;
+while (true)
+{
+
+   view.Show();
+   Console.Write("Cross turn > ");
+   turn = Console.ReadLine();
+   field.Turn(turn, CellState.Cross);
+
+   view.Show();
+   Console.Write("Zero turn > ");
+   turn = Console.ReadLine();
+   field.Turn(turn, CellState.Zero);
+
+}
+
