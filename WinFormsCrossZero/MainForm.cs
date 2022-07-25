3eaf6627aad6fb34387a14b06255f79869b50c23
volodymyr bondariev <vladmikhbond@hotmail.com>
2022-07-25 15:41:32 +0300

    .

--- WinFormsCrossZero/MainForm.cs
+++ WinFormsCrossZero/MainForm.cs
@@ -1,22 +1,43 @@
 ﻿using OopCrossZero;
+using System.Reflection;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WinFormsCrossZero
 {
 
    public partial class MainForm : Form
    {
-      const int CELL_SIZE = 40;
 
       Field model = new Field(10);
+      View view;
 
-      CellState nextTurn = CellState.Cross;
-
-      XButton[,] xbuttons;   // for viewing model
 
       public MainForm()
       {
          InitializeComponent();
+         view = new View(model, this);
+      }
+   }
 
+   class View
+   {
+      
+      const int CS = 40;  // cell size
+      Field model;
+      Form owner;
+      public XButton[,] xbuttons;
+      CellState nextTurn = CellState.Cross;
+
+      public View(Field model, Form owner)
+      {
+         this.owner = owner;
+         this.model = model;
+         InitializeView();
+      }
+
+      private void InitializeView()
+      {
          xbuttons = new XButton[model.Size, model.Size];
 
          for (int r = 0; r < model.Size; r++)
@@ -24,33 +45,39 @@ namespace WinFormsCrossZero
             for (int c = 0; c < model.Size; c++)
             {
                XButton xb = new XButton($"{r}{c}");
-               xb.Location = new Point(c * CELL_SIZE, r * CELL_SIZE);   // columns first, rows second
-               xb.Size = new Size(CELL_SIZE, CELL_SIZE);
-               Controls.Add(xb);
+               xb.Location = new Point(c * CS, r * CS);   // columns first, rows second
+               xb.Size = new Size(CS, CS);
+               owner.Controls.Add(xb);
 
-               xbuttons[r, c] = xb; 
+               xbuttons[r, c] = xb;
                xb.Click += XButton_Click;
             }
          }
-
-         this.ClientSize = new Size(model.Size * CELL_SIZE + 5, model.Size * CELL_SIZE + 5);
+         owner.ClientSize = new Size(model.Size * CS + 5, model.Size * CS + 5);
       }
+
+      private void Epilog(GameState gameState)
+      {
+         owner.Text = gameState.ToString();
+      }
+
+
 
       private void XButton_Click(object sender, EventArgs e)
       {
          XButton xb = (XButton)sender;
          GameState gameState = model.Turn(xb.TurnCoords, nextTurn);
-         
+
          switch (gameState)
          {
-            case GameState.CrossWon: 
+            case GameState.CrossWon:
             case GameState.ZeroWon:
             case GameState.Draw:
-               View(model);
+               Show();
                Epilog(gameState);
                break;
             case GameState.Continue:
-               View(model);
+               Show();
                nextTurn = (CellState)((int)CellState.Cross + (int)CellState.Zero - (int)nextTurn);
                break;
             case GameState.BadTurn:
@@ -59,7 +86,7 @@ namespace WinFormsCrossZero
 
       }
 
-      private void View(Field model)
+      private void Show()
       {
          for (int r = 0; r < model.Size; r++)
          {
@@ -72,9 +99,7 @@ namespace WinFormsCrossZero
 
       }
 
-      private void Epilog(GameState gameState)
-      {
-         Text = gameState.ToString();
-      }
+
    }
+
 }
